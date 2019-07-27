d50e8812d35b3ecca2d54a1957ee70034d2e4333
Séverin Lemaignan <severin@guakamole.org>
2019-07-27 23:21:29 +0100

    [generate_bib] deal with entries without a bibtex field (like patents)

--- generate_general_bib.py
+++ generate_general_bib.py
@@ -3,5 +3,6 @@ import json
 with open('publications.json', encoding="utf-8") as data_file:
     data = json.load(data_file)
     for entry in data:
-        bibtex = "\n\t".join(entry["bibtex"].split('\\n'))
-        print(bibtex)
+        if "bibtex" in entry:
+            bibtex = "\n\t".join(entry["bibtex"].split('\\n'))
+            print(bibtex)
